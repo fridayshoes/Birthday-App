1a5c1c320caf882b4e4022d58168707fa0ea70f3
fridayshoes <one_amp@hotmail.com>
2022-06-05 16:58:55 +0100

    Changed form months to numbers

--- spec/enter_details_spec.rb
+++ spec/enter_details_spec.rb
@@ -1,7 +1,7 @@
 feature 'Enter details' do
   scenario 'submitting details' do
     enter_details
-    expect(page).to have_content 'Jason 23 january'
+    expect(page).to have_content 'Jason 23 01'
   end
 end
 
